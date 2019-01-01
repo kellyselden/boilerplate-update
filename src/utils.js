484a12dfa7aefa43572ded5fe615600ca9e60c91
Kelly Selden <kellyselden@gmail.com>
2019-01-01 16:10:57 +0000

    add getApplicableCodemods

--- src/utils.js
+++ src/utils.js
@@ -4,8 +4,8 @@
 
 module.exports.run = require('./run');
 module.exports.npx = require('./npx');
-// module.exports.getCodemods = require('./get-codemods');
-// module.exports.getNodeVersion = require('./get-node-version');
+module.exports.getCodemods = require('./get-codemods');
+module.exports.getNodeVersion = require('./get-node-version');
 // module.exports.opn = require('opn');
 // module.exports.getApplicableCodemods = require('./get-applicable-codemods');
 module.exports.runCodemod = require('./run-codemod');
