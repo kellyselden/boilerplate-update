78884d3ae10995d83c5f03a52f01755c89185576
Kelly Selden <kellyselden@gmail.com>
2019-01-01 12:46:44 +0000

    test codemod-manifest

--- test/integration/get-codemods-test.js
+++ test/integration/get-codemods-test.js
@@ -4,7 +4,7 @@ const { expect } = require('chai');
 const co = require('co');
 const getCodemods = require('../../src/get-codemods');
 
-const url = 'https://rawgit.com/ember-cli/ember-cli-update-codemods-manifest/v2/manifest.json';
+const url = 'https://cdn.jsdelivr.net/gh/kellyselden/boilerplate-update@codemod-manifest/test/fixtures/codemod-manifest.json';
 
 describe('Integration - getCodemods', function() {
   this.timeout(5 * 1000);
