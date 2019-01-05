7002dd50ffe7b4a2b749742c652d0b25e73de8a4
Kelly Selden <kellyselden@gmail.com>
2019-01-05 11:12:16 +0000

    adds

--- test/unit/get-tag-version-test.js
+++ test/unit/get-tag-version-test.js
@@ -4,16 +4,16 @@ const { expect } = require('chai');
 const getTagVersion = require('../../src/get-tag-version');
 const sinon = require('sinon');
 const co = require('co');
-const utils = require('../../src/utils');
+const npm = require('../../src/npm');
 
 describe('Unit - getTagVersion', function() {
   let sandbox;
-  let runStub;
+  let npmJsonStub;
 
   beforeEach(function() {
     sandbox = sinon.createSandbox();
 
-    runStub = sandbox.stub(utils, 'run').resolves('"2.14.0"');
+    npmJsonStub = sandbox.stub(npm, 'json');
   });
 
   afterEach(function() {
@@ -35,7 +35,7 @@ describe('Unit - getTagVersion', function() {
   }));
 
   it('resolves dist-tags', co.wrap(function*() {
-    runStub.withArgs(sinon.match('foo@bar')).resolves('"2.14.0"');
+    npmJsonStub.withArgs(sinon.match('foo@bar')).resolves('2.14.0');
 
     expect(yield getTagVersion({
       range: 'bar',
@@ -43,6 +43,6 @@ describe('Unit - getTagVersion', function() {
       distTags: ['bar']
     })).to.equal('2.14.0');
 
-    expect(runStub.calledOnce).to.be.ok;
+    expect(npmJsonStub.calledOnce).to.be.ok;
   }));
 });
