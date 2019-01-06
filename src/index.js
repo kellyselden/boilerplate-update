05ce8e59c0d1c3557f63b44eb91d4ae8964edfca
Kelly Selden <kellyselden@gmail.com>
2019-01-06 07:19:14 +0000

    add functions

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const mergePackageJson = require('merge-package.json');
 const gitDiffApply = require('git-diff-apply');
 const run = require('./run');
 const replaceFile = require('./replace-file');
+const promptAndRunCodemods = require('./prompt-and-run-codemods');
+const getStartAndEndCommands = require('./get-start-and-end-commands');
 
 module.exports = function boilerplateUpdate({
   remoteUrl,
@@ -11,39 +13,62 @@ module.exports = function boilerplateUpdate({
   endTag,
   resolveConflicts,
   reset,
+  runCodemods,
+  codemodsUrl,
+  projectType,
+  startVersion,
   createCustomDiff,
-  startCommand,
-  endCommand
+  customDiffOptions
 }) {
-  let ignoredFiles;
-  if (!reset) {
-    ignoredFiles = ['package.json'];
-  } else {
-    ignoredFiles = [];
+  if (runCodemods) {
+    return promptAndRunCodemods({
+      url: codemodsUrl,
+      projectType,
+      startVersion
+    });
   }
 
-  return gitDiffApply({
-    remoteUrl,
-    startTag,
-    endTag,
-    resolveConflicts,
-    ignoredFiles,
-    reset,
-    createCustomDiff,
-    startCommand,
-    endCommand
-  }).then(results => {
-    if (reset) {
-      return;
+  let startCommand;
+  let endCommand;
+
+  return Promise.resolve().then(() => {
+    if (createCustomDiff) {
+      return getStartAndEndCommands(customDiffOptions).then(commands => {
+        startCommand = commands.startCommand;
+        endCommand = commands.endCommand;
+      });
+    }
+  }).then(() => {
+    let ignoredFiles;
+    if (!reset) {
+      ignoredFiles = ['package.json'];
+    } else {
+      ignoredFiles = [];
     }
 
-    let fromPackageJson = results.from['package.json'];
-    let toPackageJson = results.to['package.json'];
+    return gitDiffApply({
+      remoteUrl,
+      startTag,
+      endTag,
+      resolveConflicts,
+      ignoredFiles,
+      reset,
+      createCustomDiff,
+      startCommand,
+      endCommand
+    }).then(results => {
+      if (reset) {
+        return;
+      }
 
-    return replaceFile('package.json', myPackageJson => {
-      return mergePackageJson(myPackageJson, fromPackageJson, toPackageJson);
-    }).then(() => {
-      return run('git add package.json');
+      let fromPackageJson = results.from['package.json'];
+      let toPackageJson = results.to['package.json'];
+
+      return replaceFile('package.json', myPackageJson => {
+        return mergePackageJson(myPackageJson, fromPackageJson, toPackageJson);
+      }).then(() => {
+        return run('git add package.json');
+      });
     });
   });
 };
