1e2b5b4d6169ea28426905775631b489a3326bad
Kelly Selden <kellyselden@gmail.com>
2019-01-22 14:23:03 +0000

    add getProjectVersion

---
+++ test/unit/get-project-version-test.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const { expect } = require('chai');
+const getProjectVersion = require('../../src/get-project-version');
+
+describe('Unit - getProjectVersion', function() {
+  it('works', function() {
+    expect(getProjectVersion(
+      '2.11.1',
+      [
+        '2.11.0',
+        '2.11.1',
+        '2.11.2'
+      ]
+    )).to.equal('2.11.1');
+    expect(getProjectVersion(
+      '2.12',
+      [
+        '2.12.0',
+        '2.12.1'
+      ]
+    )).to.equal('2.12.0');
+  });
+});
