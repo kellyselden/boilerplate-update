321a41eb7a09333c4e9cf08eb9f44cc3eb8cab18
Kelly Selden <kellyselden@gmail.com>
2019-02-20 20:39:00 +0000

    co wrap getApplicableCodemods

--- src/get-applicable-codemods.js
+++ src/get-applicable-codemods.js
@@ -2,24 +2,25 @@
 
 const utils = require('./utils');
 const semver = require('semver');
+const co = require('co');
 
-module.exports = function getApplicableCodemods({
+module.exports = co.wrap(function* getApplicableCodemods({
   url,
   projectType,
   startVersion
 }) {
   let nodeVersion = utils.getNodeVersion();
 
-  return utils.getCodemods(url).then(codemods => {
-    return Object.keys(codemods).filter(codemod => {
-      codemod = codemods[codemod];
-      let isVersionInRange = semver.gte(startVersion, codemod.version);
-      let isCorrectProjectType = codemod.projectTypes.indexOf(projectType) !== -1;
-      let isNodeVersionInRange = semver.gte(nodeVersion, codemod.nodeVersion);
-      return isVersionInRange && isCorrectProjectType && isNodeVersionInRange;
-    }).reduce((applicableCodemods, codemod) => {
-      applicableCodemods[codemod] = codemods[codemod];
-      return applicableCodemods;
-    }, {});
-  });
-};
+  let codemods = yield utils.getCodemods(url);
+
+  return Object.keys(codemods).filter(codemod => {
+    codemod = codemods[codemod];
+    let isVersionInRange = semver.gte(startVersion, codemod.version);
+    let isCorrectProjectType = codemod.projectTypes.indexOf(projectType) !== -1;
+    let isNodeVersionInRange = semver.gte(nodeVersion, codemod.nodeVersion);
+    return isVersionInRange && isCorrectProjectType && isNodeVersionInRange;
+  }).reduce((applicableCodemods, codemod) => {
+    applicableCodemods[codemod] = codemods[codemod];
+    return applicableCodemods;
+  }, {});
+});
