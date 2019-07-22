cfae13fdfa4f74a7da543dbf6df65a8674a28ae1
Kelly Selden <kellyselden@gmail.com>
2019-07-22 14:09:44 +0100

    use execa.command

--- src/npx.js
+++ src/npx.js
@@ -1,12 +1,13 @@
 'use strict';
 
 const path = require('path');
-const execa = require('execa');
+const { command: _command } = require('execa');
 const debug = require('debug')('boilerplate-update');
 
 module.exports = async function npx(command, options = {}) {
-  debug(`npx ${command}`);
-  await execa('npx', command.split(' '), Object.assign({}, {
+  let npxCommand = `npx ${command}`;
+  debug(npxCommand);
+  await _command(npxCommand, Object.assign({}, {
     localDir: path.join(__dirname, '..'),
     stdio: 'inherit'
   }, options));
