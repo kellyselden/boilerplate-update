58d7a083f79cd269dc1bcecefe462e8e89feea49
Ludovic SCHMIEDER <Ludovic.Schmieder-ext@isocel.info>
2019-08-09 17:32:56 +0200

    fix: handle HTTPS request throw proxy

--- src/get-codemods.js
+++ src/get-codemods.js
@@ -1,12 +1,24 @@
 'use strict';
 
 const https = require('https');
+const HttpProxyAgent = require('https-proxy-agent');
 
 module.exports = async function getCodemods(url) {
   let manifest = '';
 
+  // support corporate firewalls
+  let proxy = process.env.https_proxy
+    || process.env.HTTPS_PROXY
+    || process.env.http_proxy
+    || process.env.HTTP_PROXY;
+
+  let httpOptions = {};
+  if (proxy) {
+    httpOptions = { agent: new HttpProxyAgent(proxy) };
+  }
+
   await new Promise((resolve, reject) => {
-    https.get(url, res => {
+    https.get(url, httpOptions, res => {
       res.on('data', d => {
         manifest += d;
       }).on('end', resolve);
