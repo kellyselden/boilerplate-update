27d92e3ed5edce61c99f400721c763f68f64ad07
Kelly Selden <kellyselden@gmail.com>
2019-08-22 14:28:26 +0100

    add codemod logging

--- src/run-codemod.js
+++ src/run-codemod.js
@@ -1,15 +1,29 @@
+/* eslint-disable no-console */
 'use strict';
 
 const utils = require('./utils');
 
 module.exports = async function runCodemod(codemod) {
+  console.log(`Running codemod ${codemod.name}`);
   if (codemod.script) {
-    await utils.runScript(codemod.script);
-    return;
-  }
-  for (let command of codemod.commands) {
     try {
-      await utils.npx(command);
-    } catch (err) {}
+      await utils.runScript(codemod.script);
+    } catch (err) {
+      console.error(`Error running script ${codemod.script}`);
+      return;
+    }
+  } else {
+    for (let i = 0; i < codemod.commands.length; i++) {
+      console.log(`Running command ${i + 1} of ${codemod.commands.length}`);
+      let command = codemod.commands[i];
+      try {
+        await utils.npx(command);
+      } catch (err) {
+        console.error(`Error running command ${command}`);
+        return;
+      }
+      console.log(`Finished running command ${i + 1} of ${codemod.commands.length}`);
+    }
   }
+  console.log(`Finished running codemod ${codemod.name}`);
 };
--- test/unit/run-codemod-test.js
+++ test/unit/run-codemod-test.js
@@ -10,72 +10,122 @@ describe(runCodemod, function() {
   let sandbox;
   let npx;
   let runScript;
+  let log;
+  let error;
 
   beforeEach(function() {
     sandbox = sinon.createSandbox();
 
     npx = sandbox.stub(utils, 'npx').resolves();
     runScript = sandbox.stub(utils, 'runScript').resolves();
+    log = sandbox.stub(console, 'log');
+    error = sandbox.stub(console, 'error');
   });
 
   afterEach(function() {
     sandbox.restore();
   });
 
-  it('runs a command', async function() {
-    await runCodemod({
-      commands: [
-        'test command'
-      ]
+  describe('command', function() {
+    it('runs a command', async function() {
+      await runCodemod({
+        name: 'test-codemod',
+        commands: [
+          'test command'
+        ]
+      });
+
+      expect(npx.args).to.deep.equal([
+        ['test command']
+      ]);
+      expect(runScript).to.not.be.called;
+
+      expect(log.withArgs('Running codemod test-codemod')).to.be.called;
+      expect(log.withArgs('Running command 1 of 1')).to.be.called;
+      expect(log.withArgs('Finished running command 1 of 1')).to.be.called;
+      expect(log.withArgs('Finished running codemod test-codemod')).to.be.called;
     });
 
-    expect(npx.args).to.deep.equal([
-      ['test command']
-    ]);
-    expect(runScript.called).to.be.false;
+    it('runs multiple commands sequentially', async function() {
+      let npx1 = npx.withArgs('test command 1').callsFake(async() => {
+        expect(npx2.args).to.deep.equal([]);
+      });
+      let npx2 = npx.withArgs('test command 2').callsFake(async() => {
+        expect(npx1.args).to.deep.equal([['test command 1']]);
+      });
+
+      await runCodemod({
+        name: 'test-codemod',
+        commands: [
+          'test command 1',
+          'test command 2'
+        ]
+      });
+
+      expect(npx.args).to.deep.equal([
+        ['test command 1'],
+        ['test command 2']
+      ]);
+
+      expect(log.withArgs('Running codemod test-codemod')).to.be.called;
+      expect(log.withArgs('Running command 1 of 2')).to.be.called;
+      expect(log.withArgs('Finished running command 1 of 2')).to.be.called;
+      expect(log.withArgs('Running command 2 of 2')).to.be.called;
+      expect(log.withArgs('Finished running command 2 of 2')).to.be.called;
+      expect(log.withArgs('Finished running codemod test-codemod')).to.be.called;
+    });
+
+    it('stops if one command errors', async function() {
+      npx.withArgs('test command 1').rejects();
+
+      await runCodemod({
+        name: 'test-codemod',
+        commands: [
+          'test command 1',
+          'test command 2'
+        ]
+      });
+
+      expect(npx.withArgs('test command 2')).to.not.be.called;
+
+      expect(log.withArgs('Running codemod test-codemod')).to.be.called;
+      expect(log.withArgs('Running command 1 of 2')).to.be.called;
+      expect(error.withArgs('Error running command test command 1')).to.be.called;
+      expect(log.withArgs('Finished running command 1 of 2')).to.not.be.called;
+      expect(log.withArgs('Running command 2 of 2')).to.not.be.called;
+      expect(log.withArgs('Finished running command 2 of 2')).to.not.be.called;
+      expect(log.withArgs('Finished running codemod test-codemod')).to.not.be.called;
+    });
   });
 
-  it('runs multiple commands sequentially', async function() {
-    let npx1 = npx.withArgs('test command 1').callsFake(async() => {
-      expect(npx2.args).to.deep.equal([]);
-    });
-    let npx2 = npx.withArgs('test command 2').callsFake(async() => {
-      expect(npx1.args).to.deep.equal([['test command 1']]);
-    });
+  describe('script', function() {
+    it('runs a script', async function() {
+      await runCodemod({
+        name: 'test-codemod',
+        script: 'test script'
+      });
 
-    await runCodemod({
-      commands: [
-        'test command 1',
-        'test command 2'
-      ]
-    });
+      expect(runScript.args).to.deep.equal([['test script']]);
+      expect(npx).to.not.be.called;
 
-    expect(npx.args).to.deep.equal([
-      ['test command 1'],
-      ['test command 2']
-    ]);
-  });
-
-  it('continues if one codemod errors', async function() {
-    npx.withArgs('test command 1').rejects();
-    let npx2 = npx.withArgs('test command 2').resolves();
-
-    await runCodemod({
-      commands: [
-        'test command 1',
-        'test command 2'
-      ]
+      expect(log.withArgs('Running codemod test-codemod')).to.be.called;
+      expect(log.withArgs('Finished running codemod test-codemod')).to.be.called;
     });
 
-    expect(npx2.calledOnce).to.be.ok;
-  });
+    it('doesn\'t throw if error', async function() {
+      runScript.rejects();
 
-  it('runs a script', async function() {
-    await runCodemod({
-      script: 'test script'
+      await runCodemod({
+        name: 'test-codemod',
+        script: 'test script'
+      });
+
+      expect(runScript.args).to.deep.equal([['test script']]);
+      expect(npx).to.not.be.called;
+
+      expect(log.withArgs('Running codemod test-codemod')).to.be.called;
+      expect(error.withArgs('Error running script test script')).to.be.called;
+      expect(log.withArgs('Finished running codemod test-codemod')).to.not.be.called;
     });
-
-    expect(runScript.args).to.deep.equal([['test script']]);
-    expect(npx.called).to.be.false;
   });
 });
