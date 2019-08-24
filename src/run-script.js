c0c62f66201d6aa2187e1d9396a7c46a9e0a3d50
Kelly Selden <kellyselden@gmail.com>
2019-08-24 13:25:52 +0100

    expose execa as a convenience

--- src/run-script.js
+++ src/run-script.js
@@ -2,6 +2,7 @@
 
 const { promisify } = require('util');
 const tmpDir = promisify(require('tmp').dir);
+const execa = require('execa');
 
 const AsyncFunction = Object.getPrototypeOf(async function() {}).constructor;
 
@@ -11,7 +12,7 @@ module.exports = async function runScript(script) {
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/eval#Do_not_ever_use_eval!
   // return eval(script);
 
-  let func = AsyncFunction('require', 'cwd', `'use strict';${script}`);
+  let func = AsyncFunction('require', 'cwd', 'execa', `'use strict';${script}`);
 
-  return await func(require, cwd);
+  return await func(require, cwd, execa);
 };
