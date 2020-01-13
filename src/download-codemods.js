7f7f2099f272b8e3d8d94da990ef5b79b9b97894
Kelly Selden <kellyselden@gmail.com>
2020-01-13 21:41:00 +0000

    warn about new codemods manifest

--- src/download-codemods.js
+++ src/download-codemods.js
@@ -4,15 +4,53 @@ const pacote = require('pacote');
 const { promisify } = require('util');
 const tmpDir = promisify(require('tmp').dir);
 const path = require('path');
+const npa = require('npm-package-arg');
 
-module.exports = async function downloadCodemods(url) {
+async function downloadAndCheckForUpdates(source) {
   let dest = await tmpDir();
 
-  await pacote.extract(url, dest);
+  let spec = npa(source);
 
-  let { main } = require(path.join(dest, 'package'));
+  let extract = pacote.extract(spec, dest);
 
-  let manifest = require(path.join(dest, main));
+  let resolve;
+
+  switch (spec.type) {
+    case 'git':
+      resolve = pacote.resolve(`${spec.fetchSpec}#semver:*`);
+      break;
+    case 'version':
+      resolve = pacote.resolve(`${spec.name}`);
+      break;
+    default:
+      resolve = (async() => (await extract).resolved)();
+      break;
+  }
+
+  let [manifest, resolved] = await Promise.all([extract, resolve]);
+
+  if (resolved !== manifest.resolved) {
+    // eslint-disable-next-line no-console
+    console.warn('There is a new version of the codemods manifest.');
+  }
+
+  return dest;
+}
+
+async function requireManifest(cwd) {
+  let { main } = require(path.join(cwd, 'package'));
+
+  return require(path.join(cwd, main));
+}
+
+async function downloadCodemods(url) {
+  let dest = await module.exports.downloadAndCheckForUpdates(url);
+
+  let manifest = module.exports.requireManifest(dest);
 
   return manifest;
-};
+}
+
+module.exports = downloadCodemods;
+module.exports.downloadAndCheckForUpdates = downloadAndCheckForUpdates;
+module.exports.requireManifest = requireManifest;
