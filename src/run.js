df81ba7f528a3b382b3f3b0576e40a6ce5dfbcf4
Kelly Selden <kellyselden@gmail.com>
2020-12-20 21:04:46 +0000

    extract spawn helper

--- src/run.js
+++ src/run.js
@@ -3,18 +3,34 @@
 const debug = require('./debug');
 const execa = require('execa');
 
+function bind(execa) {
+  return function() {
+    let ps = execa(...arguments);
+
+    ps.stdout.on('data', data => {
+      debug(data.toString());
+    });
+
+    return ps;
+  };
+}
+
+const _spawn = bind(execa);
+const _exec = bind(execa.command);
+
+function spawn(bin, args, options) {
+  debug(bin, ...args.map(arg => `"${arg}"`), options);
+
+  return _spawn(...arguments);
+}
+
 function exec() {
   debug(...arguments);
 
-  let ps = execa.command(...arguments);
-
-  ps.stdout.on('data', data => {
-    debug(data.toString());
-  });
-
-  return ps;
+  return _exec(...arguments);
 }
 
 module.exports = {
+  spawn,
   exec
 };
