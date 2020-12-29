921c5599adb0f11e2ad8da82cb447b29d1c1881a
Kelly Selden <kellyselden@gmail.com>
2020-12-29 13:03:17 +0000

    add npm tests

---
+++ test/integration/npm-test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const { describe, it } = require('../helpers/mocha');
+const { expect } = require('../helpers/chai');
+const npm = require('../../src/npm');
+
+describe(function() {
+  this.timeout(5 * 1000);
+
+  describe(npm, function() {
+    it('works', async function() {
+      let result = await npm('install -h');
+
+      expect(result).to.include('isntal');
+    });
+  });
+
+  describe(npm.json, function() {
+    it('works', async function() {
+      let result = await npm.json('v lodash');
+
+      expect(result.name).to.equal('lodash');
+    });
+  });
+});
