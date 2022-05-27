81efecd079f512d0f368d00ee84a84013946fb01
Kelly Selden <kellyselden@gmail.com>
2022-05-27 12:40:05 -0700

    add chai-semver

--- test/helpers/chai.js
+++ test/helpers/chai.js
@@ -6,5 +6,6 @@ chai.use(require('chai-fs'));
 chai.use(require('chai-as-promised'));
 chai.use(require('chai-string'));
 chai.use(require('sinon-chai'));
+chai.use(require('chai-semver'));
 
 module.exports = chai;
