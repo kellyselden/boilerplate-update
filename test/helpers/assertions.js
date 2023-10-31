a4394ebe0e35b09c3cd0b7cf9bf8e7aa307752d8
Kelly Selden <kellyselden@gmail.com>
2023-10-30 20:26:54 -0700

    remove resolveConflicts

--- test/helpers/assertions.js
+++ test/helpers/assertions.js
@@ -5,9 +5,10 @@ const { expect } = require('../helpers/chai');
 module.exports.assertNormalUpdate = function(status) {
   expect(status).to.match(/^A {2}.*added-changed\.txt$/m);
   expect(status).to.match(/^A {2}.*added-unchanged\.txt$/m);
-  expect(status).to.match(/^M {2}.*present-added-changed\.txt$/m);
-  expect(status).to.match(/^M {2}.*present-changed\.txt$/m);
-  expect(status).to.match(/^D {2}.*removed-changed\.txt$/m);
+  expect(status).to.match(/^DU .*missing-changed\.txt$/m);
+  expect(status).to.match(/^AA .*present-added-changed\.txt$/m);
+  expect(status).to.match(/^UU .*present-changed\.txt$/m);
+  expect(status).to.match(/^UD .*removed-changed\.txt$/m);
   expect(status).to.match(/^D {2}.*removed-unchanged\.txt$/m);
 };
 
@@ -19,6 +20,11 @@ module.exports.assertNoUnstaged = function(status) {
 module.exports.assertNoStaged = function(status) {
   // assert no staged changes
   expect(status).to.not.match(/^\w/m);
+};
+
+module.exports.assertAllStaged = function(status) {
+  // assert all changes staged
+  expect(status).to.not.match(/^ /m);
 };
 
 module.exports.assertCodemodRan = function(status) {
